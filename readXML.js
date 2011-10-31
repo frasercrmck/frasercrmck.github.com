5e78a9c4eefcbd2842c5cce83f1d0cb30cca4db7
Fraser Cormack <frasercrmck@gmail.com>
2011-10-31 10:12:51 +0000

    added jquery test

---
+++ readXML.js
@@ -0,0 +1,14 @@
+// File: readXML.js
+
+// Start function when DOM has completely loaded 
+$(document).ready(function(){ 
+
+	
+	var $output = '<h2>Appendation</h2>'; 
+	
+	$( "#button" ).append( $output );  
+	
+	//var $xml = "students.xml";
+	//var $xmlDoc = $.parseXML($xml);    
+});
+ 
