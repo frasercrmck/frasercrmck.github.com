5fd2fcc1df661ae17d4300e523dc028e46208ff1
Fraser Cormack <frasercrmck@gmail.com>
2011-10-31 10:23:13 +0000

    More JQuery

--- readXML.js
+++ readXML.js
@@ -6,9 +6,28 @@ $(document).ready(function(){
 	
 	var $output = '<h2>Appendation</h2>'; 
 	
-	$( "#button" ).append( $output );  
+	$( "#heading" ).append( $output );  
+	
+	$.ajax({  
+		type: "GET",
+		url: 'students.xml',  
+		dataType: "xml",  
+		success: Working , 
+		error: NotWorking
+    });
 	
 	//var $xml = "students.xml";
 	//var $xmlDoc = $.parseXML($xml);    
 });
+ 
+ function Working(){
+ 	$( "#ContentArea" ).append( "<h3>Working!</h3>" );
+ 	return;
+ }
+ 
+  function NotWorking(){
+ 	$( "#ContentArea" ).append( "<h3>Not Working!</h3>" );
+ 	return;
+ }
+ 
  
